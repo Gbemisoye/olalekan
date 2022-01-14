685343496fb46fadd0ef23eb6a261e4f03b5674c
gbemisoye <olalekanolalere1804@gmail.com>
2022-01-14 12:52:44 +0100

    week6

---
+++ Question1/Question1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Question1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BiggestNumber();
+        }
+        public static void BiggestNumber()
+        {
+            int[] input = { 3, 30, 34, 5, 9 };
+            string number = Convert.ToString(input).Substring(0,1);
+
+            // int nums = new int[number.Length];
+            int nums = Convert.ToInt32(number);
+           
+            for (int i  = 0; i  < number.Length - 1; i ++)
+            {
+                for (int j = i + 1; j < number.Length; j++)
+                {
+                    if (number[i] > number[i + 1])
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ Question2/Question2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Question2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            SubSequence();
+
+        }
+        public static void SubSequence()
+        {
+            int[] input = { 10, 9, 2, 5, 3, 7, 101, 18 };
+              
+            int count = 0;
+            //int longestCount = 0;
+            for (int i = 0; i < input.Length-1; i++)
+            {
+            
+                    if (input[i] <= input[i+1])
+                    {
+                        count++;   
+                    }              
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
